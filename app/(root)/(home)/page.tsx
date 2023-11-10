cb90dbd1bbba2e31b77ac5ae7403453df7ee9ebf
Aztarok <Aztarok2994@gmail.com>
2023-11-09 19:48:50 -0500

    Started ResourceCard

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,5 @@
+
+import ResourceCard from "@/components/ResourceCard"
 import SearchForm from "@/components/SearchForm"
 import Filters from "@/components/ui/Filters"
 import { getResources } from "@/sanity/actions"
@@ -22,6 +24,16 @@ const  Page = async () => {
                 <SearchForm />
             </section>
             <Filters />
+            <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+                Header
+                <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+                    {resources?.length > 0 ? (resources.map((resource: any) => (
+                        <ResourceCard key={resource._id} title={resource.title} id={resource._id} image={resource.image} downloadNumber={resource.views} />))
+                    ) : (<p className="body-regular text-white-400">No resources found</p>)}
+                    
+                </div>
+
+            </section>
         </main>
     )
 }
